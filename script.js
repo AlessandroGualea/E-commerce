8e7b62c7451e525ee43191a45736af62ef549aa0
AlessandroGualea <107243129+AlessandroGualea@users.noreply.github.com>
2023-06-16 13:07:25 +0000

    Prodotti

--- script.js
+++ script.js
@@ -37,3 +37,20 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });
+
+function aggiungiAlCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.innerHTML);
+  numero++;
+  carrello.innerHTML = numero;
+}
+function rimuoviDalCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.innerHTML);
+  numero--;
+  carrello.innerHTML = numero;
+}
+function svuotaCarrello() {
+  var carrello = document.getElementById("carrello");
+  carrello.innerHTML = 0;
+}
